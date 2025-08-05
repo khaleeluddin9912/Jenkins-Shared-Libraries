b034a66492d9a0892b11ef138b14e089c1a58fe7
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-08-05 22:11:40 +0530

    Update docker_run.groovy

--- vars/docker_run.groovy
+++ vars/docker_run.groovy
@@ -1,3 +1,3 @@
-def call(String dockerHubUser, String ImageName, String imageTag) {
-    sh "docker run -d ${dockerHubUser}/${ImageName}:${imageTag}"
+def call(String dockerHubUser, String port, String ImageName, String imageTag) { 
+    sh "docker run -d -p ${port} ${dockerHubUser}/${ImageName}:${imageTag}" 
 }

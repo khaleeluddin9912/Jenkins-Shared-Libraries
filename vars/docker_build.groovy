3d7aaaf500152e82188968c6d07a74f39bd4859a
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-08-08 23:17:29 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,5 @@
 def call(String projectName, String imageTag, String dockerHubUser) {
-    //sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
     sh "docker build -t ${projectName}:${imageTag} ."
+  //sh "docker build -t ${dockerHubUser}/${projectName}:${imageTag} ."
 }
+
